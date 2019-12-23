24fce533c86c75243b9fa41569edaa2a7878ccde
Schalk Venter <venterschalk@gmail.com>
2019-12-23 10:12:28 +0200

    Changes

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)"],
+  addons: [
+    "@storybook/addon-actions/register",
+    "@storybook/addon-links/register"
+  ],
+  presets: [
+    {
+      name: "@storybook/preset-create-react-app",
+      options: {
+        tsDocgenLoaderOptions: {
+          tsconfigPath: path.resolve(__dirname, "../tsconfig.json")
+        }
+      }
+    },
+    {
+      name: "@storybook/addon-docs/preset",
+      options: {
+        configureJSX: true
+      }
+    }
+  ]
+};
